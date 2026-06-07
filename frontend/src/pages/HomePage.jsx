8794a8c322134c633110e1acb0b831d9e5bc6f5f
oussamalay99 <oussamalay99@gmail.com>
2026-06-07 23:59:56 +0100

    Add example of environment variables configuration

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -46,7 +46,7 @@ const HomePage = () => {
         </div>
       </div>
       {/* PRODUCTS */}
-      <div className="mt-10">
+      <div>
         <h2 className="text-xl font-bold flex items-center gap-2 mb-4">
           <PackageIcon className="size-5 text-primary" />
           All Products
